116d98ce01a6d77fa4985669eb30455f917e2e30
shaghayegh <W0475240@campus.nscc.ca>
2026-06-18 20:52:28 -0400

    Created

---
+++ main.py
@@ -0,0 +1,16 @@
+#Username and password authorization
+password = input('Please Create a password: ')
+password2 = input('Please add your password again: ')
+
+if password != password2:
+    for i in range(3):
+        
+        print('Please try again!') 
+        password2 = input('Please add the password again: ')
+        
+        if password == password2:
+            print('Password created!')
+            break
+
+else:
+    ('Password created!')
